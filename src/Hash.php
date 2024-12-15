012c2a6be6c79a5a39d9b84700b2fef00598dc95
Ali Güçlü <aliguclutr@gmail.com>
2024-12-16 01:08:42 +0300

    Update Hash.php

--- src/Hash.php
+++ src/Hash.php
@@ -4,7 +4,7 @@ namespace Mirarus\TeamSpeakDNS;
 
 class Hash
 {
-    public static function hashedPassword($p, $N)
+    public static function ts3Login($p, $N)
     {
         $L = strtolower($N) . "ts3Login" . $p;
         $hash = hash_pbkdf2("sha512", $p, $L, 10000, 48, true);
